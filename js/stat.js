c7e0da43cff6030cf8c70a4c9847fdfcd3c2be8b
RomanVch <RomanV92@yandex.ru>
2020-09-22 13:23:44 +0500

    “static0.2”

--- js/stat.js
+++ js/stat.js
@@ -1,40 +1,36 @@
 'use strict';
 
-let CLOUD_WIDTH = 420;
-let CLOUD_HEIGHT = 270;
-let CLOUD_X = 100;
-let CLOUD_Y = 10;
-let GAP = 88;
-let POINT_X = 160;
-let POINT_Y_FONT = 260;
-let POINT_Y = 240;
-let COLUMN_WIDTH = 40;
-let COLUMN_HIGHT = 150;
-let playerss = [`Вы`, `Кекс`, `Катя`, `Игорь`];
+const CLOUD_WIDTH = 420;
+const CLOUD_HEIGHT = 270;
+const CLOUD_X = 100;
+const CLOUD_Y = 10;
+const GAP = 88;
+const POINT_X = 120;
+const POINT_Y_FONT = 260;
+const POINT_Y = 240;
+const COLUMN_WIDTH = 40;
+const COLUMN_HIGHT = 150;
+const FONT_X_URA = 115;
 
-let renderCloud = function (ctx, x, y, color) {
+const renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  let getMaxElement = function (arr) {
-    let maxElement = arr[0];
-    for (let i = 1; i < arr.length; i++) {
-      maxElement = arr[i];
-    }
-    return maxElement;
-  };
-  let namesSort = names.sort();
+  function getMaxElement(times) {
+    return Math.max.apply(null, times);
+  }
+  const namesSort = names.sort();
   renderCloud(ctx, CLOUD_X + CLOUD_Y, CLOUD_Y + CLOUD_Y, `rgba(0, 0, 0, 0.7)`);
   renderCloud(ctx, CLOUD_X, CLOUD_Y, `#fff`);
-  let maxTime = getMaxElement(times);
-  for (let i = 0; i <= playerss.length - 1; i++) {
+  const maxTime = getMaxElement(times);
+  for (let i = 0; i <= names.length - 1; i++) {
     ctx.fillStyle = `#000`;
     ctx.font = `16px PT Mono`;
-    ctx.fillText(`Ура вы победили !`, 115, 40);
-    ctx.fillText(`Cписок результатов:`, 115, 60);
-    ctx.fillText(Math.round(times[i]), 160 + (GAP * [i]), 100); // не могу понять от чего зависит положение
+    ctx.fillText(`Ура вы победили !`, FONT_X_URA, 40);
+    ctx.fillText(`Cписок результатов:`, FONT_X_URA, 60);
+    ctx.fillText(Math.round(times[i]), POINT_X + (GAP * [i]), +100); // не могу понять от чего зависит положение
     ctx.fillText(namesSort[i], POINT_X + (GAP * i), POINT_Y_FONT);
     if (i === 0) {
       ctx.fillStyle = `rgba(255, 0, 0, 1)`;
@@ -42,12 +38,14 @@ window.renderStatistics = function (ctx, names, times) {
     else {
       ctx.fillStyle = `hsl(220, ${~~(Math.random() * 100)}%, 47%)`;
     }
-    let deltaFromMax = COLUMN_HIGHT - (COLUMN_HIGHT * times[i] / maxTime);
-    let deltaHight = deltaFromMax - COLUMN_HIGHT;
+    const percentMaxTime = maxTime / 100;
+    const precentTime = times[i] / percentMaxTime;
+    const precentHeight = COLUMN_HIGHT / 100;
+    const resultHeight = precentHeight * precentTime - 20;
     ctx.fillRect(POINT_X + (GAP * i),
         POINT_Y,
         COLUMN_WIDTH,
-        deltaHight); // показывал в переписке еще одно решение но проблема в том что высота не ограничена а меньше 150 мы не можем поставить.
+        -resultHeight); // показывал в переписке еще одно решение но проблема в том что высота не ограничена а меньше 150 мы не можем поставить.
   }
 };
 
