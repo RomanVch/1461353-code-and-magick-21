ce86225b6ea15d41c80d32cc09d1e4ab4628c527
RomanVch <RomanV92@yandex.ru>
2020-09-26 16:59:46 +0500

    “render-Magic”

--- js/setup.js
+++ js/setup.js
@@ -1,17 +1,14 @@
 'use strict';
 
 const setup = document.querySelector(`#setupDel`);
-const similarWizard = document.querySelector(`#similar-wizard-template`);
+const similarWizard = document.querySelector(`#similar-wizard-template`).content
+.querySelector(`.setup-similar-item`);
 const setupBlock = document.querySelector(`#setup-similar`);
-const setupName = similarWizard.querySelector(`.setup-similar-label`);
-const setupCoatColor= similarWizard.querySelector(`.wizard-coat`);
-const setupeYesColor= similarWizard.querySelector(`.wizard-eyes`);
-const numberMagickMan = 4;
-const setupSimilar = document.querySelector(`#setupDel`);
-const setupItem = similarWizard.querySelectorAll(`.setup-similar-item`);
+const numberMagickMan = 3;
+
 
 const setupList = document.querySelector(`.setup-similar-list`);
-const nameMagickMan = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
+const nameMagickMan = [`Иван `, `Хуан Себастьян `, `Мария `, `Кристоф `, `Виктор `, `Юлия `, `Люпита `, `Вашингтон `];
 const lastNameMagickMan = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
 const coatColorMagickMan = [`rgb(101, 137, 164)`,
   `rgb(241, 43, 107)`,
@@ -25,30 +22,36 @@ const eyesColorMagickMan = [`black`,
   `yellow`,
   `green`];
 
-const magickMan = [
-  {name: nameMagickMan[window.randomNumbers(0, nameMagickMan.length)] + lastNameMagickMan[window.randomNumbers(0, lastNameMagickMan.length)],
-    coatColor: coatColorMagickMan[window.randomNumbers(0, coatColorMagickMan.length)],
-    eyesColor: eyesColorMagickMan[window.randomNumbers(0, eyesColorMagickMan.length)]
-  },
-  {name: nameMagickMan[window.randomNumbers(0, nameMagickMan.length)] + lastNameMagickMan[window.randomNumbers(0, lastNameMagickMan.length)],
-    coatColor: coatColorMagickMan[window.randomNumbers(0, coatColorMagickMan.length)],
-    eyesColor: eyesColorMagickMan[window.randomNumbers(0, eyesColorMagickMan.length)]
-  },
-  {name: nameMagickMan[window.randomNumbers(0, nameMagickMan.length)] + lastNameMagickMan[window.randomNumbers(0, lastNameMagickMan.length)],
-    coatColor: coatColorMagickMan[window.randomNumbers(0, coatColorMagickMan.length)],
-    eyesColor: eyesColorMagickMan[window.randomNumbers(0, eyesColorMagickMan.length)]
-  },
-  {name: nameMagickMan[window.randomNumbers(0, nameMagickMan.length)] + lastNameMagickMan[window.randomNumbers(0, lastNameMagickMan.length)],
-    coatColor: coatColorMagickMan[window.randomNumbers(0, coatColorMagickMan.length)],
-    eyesColor: eyesColorMagickMan[window.randomNumbers(0, eyesColorMagickMan.length)]
-  },
-];
-
-console.log(setupItem);
-
-for (let i = 0; i <= numberMagickMan -1; i++) {
-
-setupList.appendChild(setupItem.cloneNode(true));
-}
 setup.classList.remove(`hidden`);
 setupBlock.classList.remove(`hidden`);
+
+const magickMan = [
+  {name: nameMagickMan[Math.floor(window.randomNumbers(0, nameMagickMan.length - 1))] + lastNameMagickMan[Math.floor(window.randomNumbers(0, lastNameMagickMan.length - 1))],
+    coatColor: coatColorMagickMan[Math.floor(window.randomNumbers(0, coatColorMagickMan.length))],
+    eyesColor: eyesColorMagickMan[Math.floor(window.randomNumbers(0, eyesColorMagickMan.length))]
+  },
+  {name: nameMagickMan[Math.floor(window.randomNumbers(0, nameMagickMan.length - 1))] + lastNameMagickMan[Math.floor(window.randomNumbers(0, lastNameMagickMan.length - 1))],
+    coatColor: coatColorMagickMan[Math.floor(window.randomNumbers(0, coatColorMagickMan.length))],
+    eyesColor: eyesColorMagickMan[Math.floor(window.randomNumbers(0, eyesColorMagickMan.length))]
+  },
+  {name: nameMagickMan[Math.floor(window.randomNumbers(0, nameMagickMan.length - 1))] + lastNameMagickMan[Math.floor(window.randomNumbers(0, lastNameMagickMan.length - 1))],
+    coatColor: coatColorMagickMan[Math.floor(window.randomNumbers(0, coatColorMagickMan.length))],
+    eyesColor: eyesColorMagickMan[Math.floor(window.randomNumbers(0, eyesColorMagickMan.length))]
+  },
+  {name: nameMagickMan[Math.floor(window.randomNumbers(0, nameMagickMan.length - 1))] + lastNameMagickMan[Math.floor(window.randomNumbers(0, lastNameMagickMan.length - 1))],
+    coatColor: coatColorMagickMan[Math.floor(window.randomNumbers(0, coatColorMagickMan.length))],
+    eyesColor: eyesColorMagickMan[Math.floor(window.randomNumbers(0, eyesColorMagickMan.length))]
+  }
+];
+
+let renderWizard = function (wizard) {
+  let wizardElement = similarWizard.cloneNode(true);
+  wizardElement.querySelector(`.setup-similar-label`).textContent = wizard.name;
+  wizardElement.querySelector(`.wizard-coat`).style.fill = wizard.coatColor;
+  wizardElement.querySelector(`.wizard-eyes`).style.fill = wizard.eyesColor;
+  return wizardElement;
+};
+
+for (let i = 0; i <= numberMagickMan; i++) {
+  setupList.appendChild(renderWizard(magickMan[i]));
+}
